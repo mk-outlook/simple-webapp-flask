2fc8e613a409bfca7ea13735e3d1bb3376181c6b
mk-outlook <60869395+mk-outlook@users.noreply.github.com>
2020-06-18 17:01:03 +0530

    update in route

--- app.py
+++ app.py
@@ -10,9 +10,9 @@ def main():
 def hello():
     return 'I am good, how about you?'
 
-@app.route('/dumbo')
-def dumbo():
-    return 'Hey dumbo :P'
+@app.route('/wassup')
+def wassup():
+    return 'Nothing much, just doing some openshift stuff'
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8080)
